8c8cc00377badb797acdac98a57c10a0df164007
Leonardo Lopes dos Santos <leonardo.lopes@linkle.io>
2021-07-18 11:08:08 -0300

    🔩 settings do user e config

---
+++ pages/api/patchUserInfo.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios"
+
+
+export default async function handler(req, res) {
+
+  const { uid, data } = req.body
+
+  await axios({
+    method: 'PATCH',
+    url: 'https://us-central1-firetest-mvp.cloudfunctions.net/patchUserInfo',
+    data: 
+      {
+        uid: uid,
+        data: data
+      }
+    }
+  )
+
+  res.status(200).json({ name: 'John Doe' })
+}
+
